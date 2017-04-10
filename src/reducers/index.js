9719169bbe0e989e79969e63901acf5140a291c8
Tan Moy <tanmoy.pto@gmail.com>
2017-04-10 21:33:04 +0530

    added some comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+//combineReducer method ties all the reducers together
 import BooksReducer from './reducer_books';
 import ActiveBook from './reducer_active_book';
 
@@ -9,4 +10,4 @@ const rootReducer = combineReducers({
 
 export default rootReducer;
 
-//creates application state i.e wires up the reducer data to the app
+//creates application state i.e wires up the reducer data to the app application state is completely different than component state of react.
