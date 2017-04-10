fe5c26c9511f8b7740def13ba4fef0555e5acb47
Tan Moy <tanmoy.pto@gmail.com>
2017-04-10 21:08:35 +0530

    book details is now showing the title and pages on click

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function (){
     return[
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'},
-        {title: 'The Greatest Moment'}
+        {title: 'Javascript: The Good Parts', pages:500},
+        {title: 'Harry Potter', pages:800},
+        {title: 'The Dark Tower', pages:275},
+        {title: 'Eloquent Ruby', pages:1},
+        {title: 'The Greatest Moment', pages:0}
     ]
 };
